5e194c32d87ecc46089b3d5c16fd621de75ff89d
Goutemberg Pessoa <goutembergpessoa@Goutembergs-MacBook-Air.local>
2020-07-21 19:25:57 -0300

    Criacao da classe Driver

--- Teste.py
+++ Teste.py
@@ -4,21 +4,23 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-desired_caps = {
-    "deviceName": "R38M6092GWB",
-    "platformName": "Android",
-    "version" : "9.0",
-    "appPackage": "br.com.linx.bigretail.CaixaRapido",
-    "appActivity": "br.com.linx.bigretail.CaixaRapido.MainActivity",
-    "realDevice": "true",
-    "autoGrantPermissions": "true",
-    "newCommandTimeout": "60",
-    "skipDeviceInitialization": "true",
-    "ignoreUnimportantViews": "true",
+class Driver:
+    def __init__(self):
+        desired_caps = {
+            'deviceName': 'R38M6092GWB',
+            'platformName': 'Android',
+            'version' : '9.0',
+            'appPackage': 'br.com.linx.bigretail.CaixaRapido',
+            'appActivity': 'br.com.linx.bigretail.CaixaRapido.MainActivity',
+            'realDevice': 'true',
+            'autoGrantPermissions': 'true',
+            'newCommandTimeout': '60',
+            'skipDeviceInitialization': 'true',
+            'ignoreUnimportantViews': 'true',
 
-}
+        }
 
-driver = webdriver.Remote("http://localhost:4723/wd/hub", desired_caps)
+        self.instance = driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
 
 
 
